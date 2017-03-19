2408be787d8942d203e79ca3cbe0e987fc17e476
paul.kolonay <paul.kolonay@honeywell.com>
2017-03-19 14:01:38 +0000

    Modified to send data to honeywell qa system.

--- readTemp.js
+++ readTemp.js
@@ -2,18 +2,61 @@ var b = require('bonescript');
 var dateFormat = require('dateformat');
 var fs = require('fs');
 var path = require('path');
-var nimbits = require('./nimbits_module_2');
+var nimbits = require('./nimbits');
+var request = require('request')
+var constants = require('constants');
+
 
 var tempNow;
 
 var readInterval = 1000; // read input interval
-var saveInterval = 60000 * 15; // save reading to file
+var saveInterval = 60000 * 30; // save reading to file
 var file = path.join(process.cwd(), '/.temps');
 
 var temp_f;
 var temp_c;
 var mvInputAin1;
 
+var certFile = fs.readFileSync('certificates/testproxyclient.crt');
+var keyFile = fs.readFileSync('certificates/testproxyclient.key');
+
+var vm_info = {
+  host: 'qvoiceanalytics.honeywell.com',
+  port: 443,
+  customerId: 'kolonay'
+};
+
+// Get platform information and display
+var platform = b.getPlatform((x) => {
+    console.log('name = ' + x.name);
+    console.log('version = ' + x.version);
+    console.log('serialNumber = ' + x.serialNumber);
+    console.log('bonescript = ' + x.bonescript);
+    return x;
+} );
+
+function post (event, vm_info, posthandler) {
+  var URL = 'https://' + vm_info.host + ':' + vm_info.port + '/data/' + vm_info.customerId;
+  console.log('Sending message to ' + URL);
+  console.log('Message = ' + JSON.stringify(event));
+  request.post(
+    URL,
+    {   form: { event: JSON.stringify(event) },
+        strictSSL: true,
+        agentOptions: {
+            secureProtocol: 'SSLv23_method',
+            secureOptions: constants.SSL_OP_NO_SSLv3,
+            cert: certFile,
+            key: keyFile,
+        }
+    },
+    function (error, response, body) {
+      posthandler(error, response, event, body);
+    }
+  );
+};
+
+// Read the analog input pin based on the read interval
 function readLoop () {         //  create a loop function
    setTimeout(function () {    //  call a 3s setTimeout when the loop is called
       b.analogRead('P9_40', readTemp);
@@ -38,7 +81,7 @@ function printStatus(x) {
 
 	tempNow = temp_f + " degrees F";
 	console.log(dt + " " + temp_f);
-	storeReading(file, dt + " " + temp_f);
+	storeReading(file, {'time': ts_hms, 'temperature_f': temp_f});
 }
 
 setInterval(printStatus, saveInterval);
@@ -48,8 +91,15 @@ function storeReading(file, data) {
 		if (err) throw err;
 		console.log('Saved.');
 	});
-        var qString = '&id=pkolonay@gmail.com/openData&json={"d":' + temp_f + '}';
-        nimbits.performRequest('/service/v2/value', 'POST', qString);
+        messageObject = {'messages':[{'customerId':'kolonay','time':new Date(),'deviceName':platform.bonescript,'message':data,'site':{'id':'-1','name':'Default','timezone':'America/New_York'}}]};
+	post(messageObject,vm_info,function(err,res, body) {
+          if(err) {
+            console.log('sumtin wong');
+            console.log(err);
+          } else {
+            console.log(body);
+          }
+       }); 
 }
 
 
@@ -74,5 +124,5 @@ function tick() {
 
 setInterval(tick, readInterval);
 
-app.listen(3000);
+app.listen(3001);
 
